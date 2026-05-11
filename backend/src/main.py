24740d94c7a59b4ca0dbd028b10eaccffdffb1c4
Konstantin Alekseev <240989073+alekseevkv@users.noreply.github.com>
2026-05-11 11:52:57 +0300

    Add cors

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.api.v1.monitoring_task_api import router as monitoring_tasks_router
 from src.configs.app import settings
@@ -9,6 +10,16 @@ app = FastAPI(
     openapi_url="/api/openapi.json",
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True,
+)
+
 app.include_router(
-    monitoring_tasks_router, prefix="/monitoring_tasks", tags=["Задачи мониторинга"]
+    monitoring_tasks_router,
+    prefix="/api/v1/monitoring_tasks",
+    tags=["Задачи мониторинга"],
 )
