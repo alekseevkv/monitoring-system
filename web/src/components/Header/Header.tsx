f95577d23b54d8794078f04e4f16be55b6bae9ea
Konstantin Alekseev <240989073+alekseevkv@users.noreply.github.com>
2026-05-11 11:11:46 +0300

    Add navbar

--- web/src/components/Header/Header.tsx
+++ web/src/components/Header/Header.tsx
@@ -1,5 +1,45 @@
+import { Link, NavLink as RouterNavLink, useMatch } from 'react-router';
+
+import { Anchor, NavLink } from '@mantine/core';
+import { HeartbeatIcon } from '@phosphor-icons/react';
+
 import classes from './Header.module.css';
 
 export const Header = () => {
-  return <header className={classes.header}></header>;
+  const isTasksPage = useMatch('/tasks');
+  const isReportPage = useMatch('/report');
+
+  return (
+    <header className={classes.header}>
+      <div className={classes.titleWrapper}>
+        <HeartbeatIcon size={20} />
+        <Anchor
+          component={Link}
+          to="/"
+          underline="never"
+          className={classes.title}
+        >
+          Система мониторинга сервисов
+        </Anchor>
+      </div>
+      <div className={classes.navbar}>
+        <NavLink
+          component={RouterNavLink}
+          to="/tasks"
+          label="Сервисы мониторинга"
+          variant="subtle"
+          active={Boolean(isTasksPage)}
+          className={classes.link}
+        />
+        <NavLink
+          component={RouterNavLink}
+          to="/report"
+          label="Сводный отчет"
+          variant="subtle"
+          active={Boolean(isReportPage)}
+          className={classes.link}
+        />
+      </div>
+    </header>
+  );
 };
